590e2a9a8946de482455ec878188a2a4175fcd42
xw202 <xw202@duke.edu>
2020-09-14 19:20:06 +0800

    029

---
+++ 029_power_rec/power.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+unsigned power(unsigned x, unsigned y){
+  if(x==0&&y==0)
+    return 1;
+  else if(y==0){
+    return 1;
+  }
+  else{
+    return x*power(x,y-1);
+  }
+}
