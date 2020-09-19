9e68f10f30f4c818b6e9eeadfe92f3779b5ded13
xw202 <xw202@duke.edu>
2020-09-19 23:47:45 +0800

    037

---
+++ 037_array_subseq/maxSeq.c
@@ -0,0 +1,21 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+size_t maxSeq(int *array,size_t n){
+  if(n==0){
+    return 0;
+  }
+  else{
+    int max=1;
+    int cur=1;
+    for(int i=1;i<n;i++){
+      if(array[i]>array[i-1]){
+	cur++;
+      }
+      else{
+	max=(cur>max) ? cur:max;
+	cur=0;
+      }
+    }
+  }
+}
