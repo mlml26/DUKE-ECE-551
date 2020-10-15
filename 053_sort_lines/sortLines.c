a2eacc5af9f22863f341485e34a646710affb7f6
xw202 <xw202@duke.edu>
2020-10-15 15:00:39 +0800

    053

--- 053_sort_lines/sortLines.c
+++ 053_sort_lines/sortLines.c
@@ -18,7 +18,55 @@ void sortData(char ** data, size_t count) {
 
 int main(int argc, char ** argv) {
   
-  //WRITE YOUR CODE HERE!
-  
+  //WRITE YOUR CODE HERE
+  if(argc==1){
+    char **lines = NULL;
+    char *curr = NULL;
+    size_t sz;
+    size_t i = 0;
+    while(getline(&curr, &sz, stdin) >= 0){
+      lines = realloc(lines, (i+1) * sizeof(*lines));
+      lines[i] = curr;
+      curr = NULL;
+      i++;
+    }
+    free(curr);
+    sortData(lines,i);
+    for(size_t j =0; j < i; j++){
+      printf("%s",lines[j]);
+      free(lines[j]);
+    }
+    free(lines);
+  }
+  else if(argc > 1){
+    for(int i =1; i< argc; i++){
+      char **lines = NULL;
+      char *curr = NULL;
+      size_t sz;
+      size_t i = 0;
+      FILE *f = fopen(argv[i],"r");
+      if(f==NULL){
+	perror("Failed to open the input file");
+	return EXIT_FAILURE;
+      }
+      while(getline(&curr, &sz, f) >= 0){
+      lines = realloc(lines, (i+1) * sizeof(*lines));
+      lines[i] = curr;
+      curr = NULL;
+      i++;
+    }
+    free(curr);
+    sortData(lines,i);
+    for(size_t j =0; j < i; j++){
+      printf("%s",lines[j]);
+      free(lines[j]);
+    }
+    free(lines);
+      if(fclose(f)!=0){
+	perror("Failed to close the input file!");
+	return EXIT_FAILURE;
+      }
+    }
+  }
   return EXIT_SUCCESS;
 }
