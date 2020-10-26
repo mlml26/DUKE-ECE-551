d71a6808c5301ea0b4eec40daeb6864ff08c4bc1
xw202 <xw202@duke.edu>
2020-10-26 20:10:06 +0800

    064

--- 064_vector_2d/vector.hpp
+++ 064_vector_2d/vector.hpp
@@ -1,3 +1,21 @@
 /* write your class interface in this file
    write function definitions only if they are very short
  */
+#include <cmath>
+class Vector2D{
+private:
+  double x;
+  double y;
+public:
+  void initVector(double init_x, double init_y){
+    x = init_x;
+    y = init_y;
+  }
+  double getMagnitude() const{
+    return std::sqrt(x * x + y * y);
+  }
+  Vector2D operator+(const Vector2D & rhs) const;
+  Vector2D & operator+=(const Vector2D & rhs);
+  double dot(const Vector2D & rhs) const;
+  void print() const;
+};
