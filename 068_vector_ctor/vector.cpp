0fa9df79ead1e97f18e1c31e25228a23cba21bcf
xw202 <xw202@duke.edu>
2020-10-28 20:50:09 +0800

    068

---
+++ 068_vector_ctor/vector.cpp
@@ -0,0 +1,24 @@
+#include "vector.hpp"
+
+#include <cmath>
+#include <cstdio>
+
+/* write your class implementation in this file
+ */
+Vector2D Vector2D::operator+(const Vector2D & rhs) const{
+  Vector2D answer;
+  answer.x = x + rhs.x;
+  answer.y = y + rhs.y;
+  return answer;
+}
+Vector2D & Vector2D::operator+=(const Vector2D & rhs){
+  x += rhs.x;
+  y += rhs.y;
+  return *this;
+}
+double Vector2D::dot(const Vector2D & rhs) const{
+  return x * rhs.x + y * rhs.y;
+}
+void Vector2D::print() const{
+  std::printf("<%.2f, %.2f>", x, y);
+}
---
+++ 068_vector_ctor/vector.hpp
@@ -0,0 +1,19 @@
+/* write your class interface in this file
+   write function definitions only if they are very short
+ */
+#include <cmath>
+class Vector2D{
+private:
+  double x;
+  double y;
+public:
+  Vector2D():x(0), y(0) {}
+  Vector2D(double init_x, double init_y):x(init_x), y(init_y){}
+  double getMagnitude() const{
+    return std::sqrt(x * x + y * y);
+  }
+  Vector2D operator+(const Vector2D & rhs) const;
+  Vector2D & operator+=(const Vector2D & rhs);
+  double dot(const Vector2D & rhs) const;
+  void print() const;
+};
