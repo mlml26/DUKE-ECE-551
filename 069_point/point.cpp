bf62d61c97b6742f311f4a2dcb65b3254d89f1b6
xw202 <xw202@duke.edu>
2020-10-28 21:29:30 +0800

    069

--- 069_point/point.cpp
+++ 069_point/point.cpp
@@ -1,6 +1,6 @@
 #include <cmath>
 #include "point.hpp"
 
-Double Point::distanceFrom(const Point & p){
+double Point::distanceFrom(const Point & p){
   return std::sqrt((p.x - x) * (p.x - x) + (p.y - y) * (p.y - y));
 }
