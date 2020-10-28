560804a6d609b1b9e09435034553b749de2361b8
xw202 <xw202@duke.edu>
2020-10-28 22:52:25 +0800

    070

---
+++ 070_circle/circle.cpp
@@ -0,0 +1,24 @@
+#include "circle.hpp"
+#include <cmath>
+#include <algorithm>
+
+void Circle::move(double dx, double dy){
+  c.move(dx,dy);
+}
+double Circle::intersectionArea(const Circle & otherCircle){
+  double d = c.distanceFrom(otherCircle.c);
+  if(d >= (r + otherCircle.r)){
+    return 0.0;
+  }
+  if(d <= abs(r - otherCircle.r)){
+    double mini = std::min(r, otherCircle.r);
+    return (mini * mini);
+  }
+  double a = r * r;
+  double b = otherCircle.r * otherCircle.r;
+  double x = (a - b + (d * d)) / (2 * d);
+  double z = x * x;
+  double y = sqrt(a - z);
+  double result = a * asin(y / r) + (b * asin(y / otherCircle.r)) - y * (x + sqrt(z + b - a));
+  return result;
+}
