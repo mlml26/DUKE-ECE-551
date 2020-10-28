fd9924e92fc76c38396bd536b38833a4b0cfa7f2
xw202 <xw202@duke.edu>
2020-10-28 23:49:29 +0800

    070

--- 070_circle/circle.cpp
+++ 070_circle/circle.cpp
@@ -1,24 +1,31 @@
 #include "circle.hpp"
 #include <cmath>
 #include <algorithm>
-
+#include <cstdio>
 void Circle::move(double dx, double dy){
   c.move(dx,dy);
 }
 double Circle::intersectionArea(const Circle & otherCircle){
+  //std::printf("%d",1);
   double d = c.distanceFrom(otherCircle.c);
   if(d > (r + otherCircle.r)){
     return 0.0;
   }
-  if(d <= abs(r - otherCircle.r)){
+  if(d <= std::abs(r - otherCircle.r)){
     double mini = std::min(r, otherCircle.r);
     return (M_PI * mini * mini);
   }
+  
   double a = r * r;
   double b = otherCircle.r * otherCircle.r;
+  /*
   double x = (a - b + (d * d)) / (2 * d);
   double z = x * x;
-  double y = sqrt(a - z);
-  double result = a * asin(y / r) + (b * asin(y / otherCircle.r)) - y * (x + sqrt(z + b - a));
+  double y = std::sqrt(a - z);
+  double result = a * asin(y / r) + (b * asin(y / otherCircle.r)) - y * (x + std::sqrt(z + b - a));
   return result;
+  */
+  double a1 = acos((a - b + d * d) / (2 * d * r));
+  double a2 = acos((b - a + d * d) / (2 * d * otherCircle.r));
+  return a1 * a + a2 * b - d * r * sin(a1);
 }
--- 070_circle/circle.hpp
+++ 070_circle/circle.hpp
@@ -4,7 +4,7 @@ private:
   Point c;
   const double r;
 public:
-  Circle(Point c_init, double r_init): c(c_init), r(r_init) {}
+  Circle(const Point & c_init, double r_init): c(c_init), r(r_init) {}
   void move(double dx, double dy);
   double intersectionArea(const Circle & otherCircle);
 };
