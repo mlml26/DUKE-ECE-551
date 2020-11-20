8ff0ab34bda7744cebde81ec78c9aadacce7526f
xw202 <xw202@duke.edu>
2020-11-20 17:27:35 +0800

    097

--- 097_binsrch/search.cpp
+++ 097_binsrch/search.cpp
@@ -1,6 +1,7 @@
 #include "function.h"
 int binarySearchForZero(Function<int, int> * f, int low, int high){
   //all negative
+  /*
   if(f->invoke(high - 1) < 0){
     return high - 1;
   }
@@ -11,7 +12,10 @@ int binarySearchForZero(Function<int, int> * f, int low, int high){
   if(low == high){
     return low;
   }
-  int cur = ((high - low) / 2) + low;
+  //int cur = ((high - low) / 2) + low;
+  */  
+/*
+  int cur = (hight + low - 1) / 2;
   if(f->invoke(cur) > 0){
     return binarySearchForZero(f, low, cur);
   }
@@ -21,4 +25,19 @@ int binarySearchForZero(Function<int, int> * f, int low, int high){
   else{
     return cur;
   }
+  */
+  while(low+1 < high){ 
+    int cur = (high + low) / 2;
+    int ans = f->invoke(cur);
+    if(ans == 0){
+      return cur;
+    }
+    if(ans > 0){
+      high = cur;
+    }
+    else{
+      low = cur;
+    }
+  }
+  return low;
 }
