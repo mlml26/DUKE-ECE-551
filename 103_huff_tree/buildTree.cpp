fcd25ce1064a978ab3c012f884221de079013a03
xw202 <xw202@duke.edu>
2020-11-25 20:44:56 +0800

    103

--- 103_huff_tree/buildTree.cpp
+++ 103_huff_tree/buildTree.cpp
@@ -1,5 +1,23 @@
 #include "node.h"
-
+#include<stdio.h>
+#include<cstdio>
 Node *  buildTree(uint64_t * counts) {
   //WRITE ME!
+  priority_queue_t pq;
+  for(int i =0; i<257;i++){
+    if(counts[i] !=0){
+      pq.push(new Node(i, counts[i]));
+    }
+  }
+  while(pq.size()!=1){
+    Node * nodeOne = pq.top();
+    pq.pop();
+    Node * nodeTwo = pq.top();
+    pq.pop();
+    Node * combine = new Node(nodeOne, nodeTwo);
+    pq.push(combine);
+  }
+  Node * root = pq.top();
+  pq.pop();
+  return root;
 }  
